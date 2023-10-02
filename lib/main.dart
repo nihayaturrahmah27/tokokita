a970c1e9b2d0c7e85f2cffcea43d2299a407c1a6
nihayaturrahmah27 <afspraak.hyrm@gmail.com>
2023-10-02 20:22:32 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+import 'package:tokokita/model/login.dart';
+import 'package:tokokita/ui/login_page.dart';
+import 'package:tokokita/ui/produk_page.dart';
+import 'package:tokokita/ui/produk_detail.dart';
+import 'package:tokokita/ui/produk_form.dart';
+import 'package:tokokita/ui/registrasi_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Toko Kita',
+      debugShowCheckedModeBanner: false,
+      home: ProdukPage(),
+    );
+  }
+}
